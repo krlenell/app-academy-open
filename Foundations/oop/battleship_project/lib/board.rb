df490fc9fff52a79e85431695e1e6ae9a025aa04
Kevin Lenell <krlenell@gmail.com>
2021-02-23 15:36:28 -0800

    finished p1 of board spec

--- Foundations/oop/battleship_project/lib/board.rb
+++ Foundations/oop/battleship_project/lib/board.rb
@@ -1,3 +1,30 @@
 class Board
-  
+  attr_reader :size
+
+  def initialize(n)
+    @grid = Array.new(n) {Array.new(n, :N)}
+    @size = n * n
+  end
+
+  def [](arr)
+    row, column = arr
+    @grid[row][column]
+  end
+
+  def []=(pos, val)
+    row, column = pos
+    @grid[row][column] = val
+  end
+
+  def num_ships
+    count = 0
+    @grid.each do |arr|
+      arr.each {|item| count += 1 if item == :S}
+    end
+    count
+  end
+
+
+
+
 end
