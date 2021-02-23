701db5c1334034ac9603459f7115327b73d4128f
Kevin Lenell <krlenell@gmail.com>
2021-02-22 22:48:29 -0800

    completed hotel project

--- Foundations/in-out/hotel_project/lib/hotel.rb
+++ Foundations/in-out/hotel_project/lib/hotel.rb
@@ -1,5 +1,47 @@
 require_relative "room"
 
 class Hotel
-  
+  def initialize(name, hash)
+    @name = name
+    @rooms = {}
+
+    hash.each do |r_name, cap|
+      @rooms[r_name] = Room.new(cap)
+    end
+  end
+
+  def name
+    @name.split.map(&:capitalize).join(" ")
+  end
+
+  def rooms
+    @rooms
+  end
+
+  def room_exists?(name)
+    @rooms.has_key?(name)
+  end
+
+  def check_in(person, name)
+    if !room_exists?(name)
+      p "sorry, room does not exist"
+    else
+      success = @rooms[name].add_occupant(person)
+      if success
+        p "check in successful"
+      else
+        p "sorry, room is full"
+      end
+    end
+  end
+
+  def has_vacancy?
+    @rooms.values.any? {|room| !room.full?}
+  end
+
+  def list_rooms
+    @rooms.each do |name, room|
+      puts "#{name}: #{room.available_space}"
+    end
+  end
 end
