6ed0f1650b642ddc49d9512de23abb659463fdf3
Kevin Lenell <krlenell@gmail.com>
2021-02-24 00:26:34 -0800

    completed enum proj

--- Ruby/Enumerables-and-Debugging/enumberables-proj/enum.rb
+++ Ruby/Enumerables-and-Debugging/enumberables-proj/enum.rb
@@ -31,4 +31,62 @@ class Array
     false
   end
 
+  def my_flatten
+    flattened = []
+
+    self.my_each do |el|
+      if el.is_a?(Array)
+        flattened.concat(el.flatten)
+      else
+        flattened << el
+      end
+    end
+
+    flattened
+  end
+
+  def my_zip(*arrays)
+    zip = []
+
+    self.length.times do |i|
+      subarray = [self[i]]
+
+      arrays.my_each do |array|
+        subarray << array[i]
+      end
+
+      zip << subarray
+    end
+    zip
+  end
+
+  # pretty sure this is wrong
+  def my_rotate(pos=1)
+    split = pos % self.length
+
+    self.drop(split) + self.take(split)
+  end
+
+  def my_join(char='')
+    res = "#{self[0]}"
+
+    (1..self.length).each do |i|
+      res += char
+      res += self[i]
+    end
+    res
+  end
+
+  def my_reverse
+    res = []
+    self.my_each do |el|
+      res.unshift el
+    end
+    res
+  end
+
+
+
+
+
 end
