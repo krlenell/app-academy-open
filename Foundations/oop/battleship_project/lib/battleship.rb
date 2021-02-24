53983d27ccf085a924c85b1582f4671e5a431efb
Kevin Lenell <krlenell@gmail.com>
2021-02-23 18:09:49 -0800

    completed battleship.rb

--- Foundations/oop/battleship_project/lib/battleship.rb
+++ Foundations/oop/battleship_project/lib/battleship.rb
@@ -2,5 +2,46 @@ require_relative "board"
 require_relative "player"
 
 class Battleship
+  attr_reader :board, :player
 
+  def initialize(n)
+    @player = Player.new
+    @board = Board.new(n)
+    @remaining_misses = @board.size / 2
+  end
+
+  def start_game
+    @board.place_random_ships
+    p "there are #{@board.num_ships} ships on the board"
+    @board.print
+  end
+
+  def lose?
+    if @remaining_misses <= 0
+      p 'you lose'
+      true
+    else
+      false
+    end
+  end
+
+  def win?
+    if @board.num_ships == 0
+      p 'you win'
+      true
+    else
+      false
+    end
+  end
+
+  def game_over?
+    win? || lose?
+  end
+
+  def turn
+    pos = @player.get_move
+    @remaining_misses -= 1 if !@board.attack(pos)
+    puts "remaining misses: #{@remaining_misses}"
+    @board.print
+  end
 end
