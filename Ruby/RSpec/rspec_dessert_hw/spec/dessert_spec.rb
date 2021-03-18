50ff31b95eb8cab1eb0f1cb4d83a95007734e178
Kevin Lenell <krlenell@gmail.com>
2021-03-17 17:19:42 -0700

    some tests created for dessert class

--- Ruby/RSpec/rspec_dessert_hw/spec/dessert_spec.rb
+++ Ruby/RSpec/rspec_dessert_hw/spec/dessert_spec.rb
@@ -5,19 +5,35 @@ require 'dessert'
 
 describe Dessert do
   let(:chef) { double('chef') }
+  # subject(:dessert { Dessert.new(chef)}
+  let(:dessert) { described_class.new('Ice Cream', 4, chef) }
 
   describe '#initialize' do
-    it 'sets a type'
 
-    it 'sets a quantity'
+    it 'sets a type' do
+      expect(dessert.type).to eq('Ice Cream')
+    end
 
-    it 'starts ingredients as an empty array'
+    it 'sets a quantity' do
+      expect(dessert.quantity).to eq(4)
+    end
+
+    it 'starts ingredients as an empty array' do
+      expect(dessert.ingredients).to be_a(Array)
+      expect(dessert.ingredients.length).to eq(0)
+    end
 
     it 'raises an argument error when given a non-integer quantity'
+      # dessert.
   end
 
   describe '#add_ingredient' do
-    it 'adds an ingredient to the ingredients array'
+    it 'adds an ingredient to the ingredients array' do
+      dessert.add_ingredient('Ice')
+      expect(dessert.ingredients[0]).to eq('Ice')
+      dessert.add_ingredient('Cream')
+      expect(dessert.ingredients[1]).to eq('Cream')
+    end
   end
 
   describe '#mix!' do
